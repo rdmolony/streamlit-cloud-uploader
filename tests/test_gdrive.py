bfbbebd0bc2220e4f677e54e912e409753c92def
Rowan Molony <rowan.molony@codema.ie>
2021-07-02 12:13:14 +0100

    Test gdrive downloader downloads sample file
    Am making a request to GDrive every time this test suite is run,
    could refactor this in future to use
    [responses](https://github.com/getsentry/responses) instead

--- tests/test_gdrive.py
+++ tests/test_gdrive.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from streamlit_cloud_uploader import gdrive
 
 
@@ -6,3 +8,12 @@ def test_extract_id_from_google_drive_link():
     expected_output = "1mzxpZS_nKx8pOLNLDO2SXzboVTE4rlV-"
     output = gdrive._extract_id_from_google_drive_link(url=input_url)
     assert output == expected_output
+
+
+def test_download_file_from_google_drive(tmp_path: Path):
+    filepath = tmp_path / "small-file.csv"
+    gdrive.download_file_from_google_drive(
+        url="https://drive.google.com/file/d/14rV7E90MgXUd9pxhdas7TyYsDXbPmP2-/view?usp=sharing",
+        filepath=filepath,
+    )
+    assert filepath.exists()
